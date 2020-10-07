1e0af642bef1cb9afab6270f08758c3a26883ba7
emperor9199 <199emperor@gmail.com>
2020-10-07 10:54:33 +0530

    useRef useState useEffect

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const refVar = useRef();
+  const [counter1, setCounter1] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+
+  useEffect(() => {
+    console.log(refVar);
+  }, [counter1, counter2]);
+
   return (
-    <h1>
-      Goto <Link to="/about">About</Link>
-    </h1>
+    <div ref={refVar}>
+      <h1>
+        Goto <Link to="/about">About</Link>
+      </h1>
+      <h1 onClick={() => setCounter1(counter1 + 1)}>Counter1 {counter1}</h1>
+      <h1 onClick={() => setCounter2(counter2 + 1)}>Counter2 {counter2}</h1>
+    </div>
   );
 };
 
